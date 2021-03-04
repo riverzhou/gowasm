d1ef07895c57aeec30f0f4d40b65ce7fe7910396
River Zhou <riverzhou2000@163.com>
2021-03-04 18:50:18 +0800

    V4.1: 架构调整完成，待测试

--- lute/lutewasm.go
+++ lute/lutewasm.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	//"github.com/riverzhou/lute"
+	"github.com/riverzhou/lute"
 )
 
 func main() {
-	//luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
-	//html:= luteEngine.MarkdownStr("demo", "**Lute** - A structured markdown engine.")
-	html := "<p><strong>Lute</strong> - A structured Markdown engine.</p>"
+	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
+	html:= luteEngine.MarkdownStr("demo", "**Lute** - A structured markdown engine.")
+	//html := "<p><strong>Lute</strong> - A structured Markdown engine.</p>"
 	println(html)
 	// <p><strong>Lute</strong> - A structured Markdown engine.</p>
 }
